3e6ff7f667516b3b4aa345639b8976a8a2fb8341
MaximeKELI <presidentnetero01@gmail.com>
2025-03-22 22:00:24 +0000

    C# game

---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace MorpionGraphique.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
